e9288149ecf598475614872bd1fceaa308abf35b
Yukine213 <kondratenkov1109@gmail.com>
2023-07-22 13:40:13 +0300

    update about me section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,23 +12,23 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius saepe
-          aliquid aperiam numquam ducimus, atque alias, velit quae aspernatur at
-          autem molestiae suscipit totam doloribus id cumque accusamus iusto
-          inventore animi dolores consequatur, quia hic! Ipsum tempore, facilis
-          officia odio officiis deserunt assumenda maxime temporibus explicabo
-          voluptatem dignissimos nulla vero!
+          Greetings! I'm Vitaliy, a dedicated and enthusiastic frontend web
+          developer, deeply enamored by the captivating world of web
+          development. With nearly 5 months of hands-on experience, I'm
+          continually fueled by my passion for creating exceptional digital
+          experiences and my insatiable appetite for learning. Since embarking
+          on my journey into web development, I've found myself utterly absorbed
+          in the art of crafting visually stunning and user-friendly websites
+          and web applications. My love for this field lies not only in the
+          technical intricacies but also in the opportunity to touch the lives
+          of people worldwide through the wonders of the internet.
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum amet,
-          sunt voluptates esse magnam ex quaerat totam vel cum distinctio ipsam?
-          A, deleniti. Quia distinctio sit eos magni tempore ullam. Corporis
-          dignissimos temporibus quidem nisi quo veniam ab commodi quae ex
-          tenetur repellendus eum placeat, ea, aspernatur, distinctio
-          blanditiis. Excepturi?
+          Aside from coding, I love to learn new things, especially involving
+          web development, watch anime and read manhwa.
         </p>
       </div>
     </div>
